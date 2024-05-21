8307e747f899075be0e2fd2c8ad7b33a2601d603
Lidia Haile <lidiahaile@gmail.com>
2024-05-21 06:48:15 +0100

    class responsable for displaying the result of the guess added

---
+++ src/DisplayResult.java
@@ -0,0 +1,48 @@
+public class DisplayResult {
+    private String wordSelected;
+    private char[] currentGuess;
+    private int lives;
+
+    public DisplayResult(String selectedWord, int lives) {
+        this.wordSelected = wordSelected;
+        this.lives = lives;
+        currentGuess = new char[wordSelected.length()];
+        for (int i = 0; i < currentGuess.length; i++) {
+            currentGuess[i] = '_';
+        }
+    }
+
+
+    public void updateDisplayResult(char guess) {
+        for (int i = 0; i < wordSelected.length(); i++) {
+            if (wordSelected.charAt(i) == guess) {
+                currentGuess[i] = guess;
+            }
+        }
+    }
+
+
+
+    public void showDisplayResult() {
+        System.out.println("Current Guess: " + new String(currentGuess));
+        System.out.println("Remaining Lives: " + lives);
+    }
+
+    public void decrementLives() {
+        lives--;
+    }
+
+    public boolean isWordGuessed() {
+        for (char c : currentGuess) {
+            if (c == '_') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isOutOfLives() {
+        return lives <= 0;
+    }
+}
+
